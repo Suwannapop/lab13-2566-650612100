1c954f67aeac65a334ddb3f2ac9e3df9699c10f3
Suppakron <suwannapop01@gmail.com>
2023-08-07 15:56:22 +0700

    fisht

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            fullName="Suppakorn Suwannapop"
+            studentId="650612100"
           />
         </div>
       </body>
