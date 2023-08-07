e7288fa10203b24a4a57e1120d2b0ecc7036b9f5
Suppakron <suwannapop01@gmail.com>
2023-08-07 15:00:22 +0700

    you good

--- src/app/search/[searchInput]/page.js
+++ src/app/search/[searchInput]/page.js
@@ -3,7 +3,7 @@
 import { MovieRow } from "@/components/MovieRow";
 import { movieDB } from "@/libs/movieDB";
 
-export default function SearchResultPage({}) {
+export default function SearchResultPage() {
   //tip1 : before filtering movie, replace all "%20" with " " (space) in the input
   // const processedSearchInput = ...
 
